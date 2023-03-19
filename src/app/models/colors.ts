1c696b4726548c3c1e256c4bc7f8c166174d49fa
Morgan Nicol <morganichoui@gmail.com>
2023-03-19 14:05:11 +0100

    bla

--- src/app/models/colors.ts
+++ src/app/models/colors.ts
@@ -1,5 +1,5 @@
 export const STATUS_BAR_COLOR = '#69BFAF';
-export const WEATHER_NEXT_HOUR_CHART_COLOR = '#8CFFEA'; // secondary
+export const WEATHER_NEXT_HOUR_CHART_COLOR = '#69BFAF'; // secondary
 export const MAIN_TEXT_COLOR = '#F3F3F3'; // ion-color-950
 
 export const FORECAST_COLOR_GRAY = '#949494';
